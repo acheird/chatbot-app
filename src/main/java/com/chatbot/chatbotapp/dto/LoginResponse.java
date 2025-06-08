29995f1b8bb93ea68f41eb0be9c1006747609de4
acheird <acheird@gmail.com>
2025-06-08 12:50:20 +0300

    add token in LoginResponse

--- src/main/java/com/chatbot/chatbotapp/dto/LoginResponse.java
+++ src/main/java/com/chatbot/chatbotapp/dto/LoginResponse.java
@@ -4,13 +4,15 @@ public class LoginResponse {
     private Long id;
     private String name;
     private String email;
+    private String token;
 
     public LoginResponse() {}
 
-    public LoginResponse(Long id, String name, String email) {
+    public LoginResponse(Long id, String name, String email, String token) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.token = token;
     }
 
     public Long getId() { return id; }
@@ -21,4 +23,7 @@ public class LoginResponse {
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
 }
